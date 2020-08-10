028dcc11f0c34b5dcfca6098ae5c1a54f5387f84
sjsanc <sjsanc@protonmail.com>
2020-08-10 20:35:54 +0100

    Added daemon & sparse -h flag

--- README.md
+++ README.md
@@ -8,16 +8,17 @@ Triggers a notification which can be turned off with the flag **-q**
 
 Example:
 
-> python dl_clean.py "path/to/listen/\*"
+> python dl_clean.py "path/to/listen/\*" -q
 
 Changelog:
 v0.1 - Just moves image files & pdfs
 v0.0.2 - Moves files according to rules set in dl_clean.conf. Also has a debug & quiet mode.
+v0.0.3 -
 
 Todo:
 
 1. Improve CLI usability with argparse & man page
-2. Daemonise properly rather than setting to timeout
+2. ~~Daemonise properly rather than setting to timeout~~
 3. Search function for looking for .conf, currently has to be in run directory
 4. Log output flag, i.e. --verbose
 5. More flexible config for locations. Currently home dir is assumed as output location
--- dl_clean.py
+++ dl_clean.py
@@ -3,6 +3,7 @@ import glob
 import os
 import sys
 import configparser
+import daemon
 
 # import config.ini file
 config = configparser.ConfigParser()
@@ -12,38 +13,49 @@ config.read("dl_clean.conf")
 quiet = False
 if "-q" in sys.argv:
     quiet = True
-    print ("dl_clean started in quiet mode")
 
 # if debug flag, output information about unmoved files
 debug = False
-if "-debug" in sys.argv:
+if "-d" in sys.argv:
     debug = True
-    print ("dl_clean started in debug mode")
+
+if sys.argv[1] == "-h":
+    print("""
+    usage: dl_clean.py 'path/to/dir' [-q] [-debug]
+    
+    Watch a directory and sort incoming files accordingly.
+
+    -h, show this help message and exit
+    -q, prevent notifications per move
+    -d, print information on missing rules
+    """
+    )
+    sys.exit()
 
 filetypes = list(config["ext"]) # get filetypes defined in config.ini
 
 def move(path):
-    # takes the watched directory as first argument
-    watched = glob.glob(path) # get contents of watched dir
-    if len(watched) == 0:
-        pass # pass on empty dir
-    else: 
-        for file in watched: 
-            filename = os.path.basename(file) # file.txt
-            name, extension = os.path.splitext(file) # (file, .txt)
+    while True:
+        # takes the watched directory as first argument
+        watched = glob.glob(path) # get contents of watched dir
+        if len(watched) == 0:
+            pass # pass on empty dir
+        else: 
+            for file in watched: 
+                filename = os.path.basename(file) # file.txt
+                name, extension = os.path.splitext(file) # (file, .txt)
 
-            if extension in filetypes: # check for existing rule in config
-                os.rename(file, os.environ["HOME"] + config["ext"][extension] + filename)
-                if quiet == False: # -q flag
-                    os.system("notify-send " + config["ext"][extension] + filename) 
-            else: 
-                if debug == True:
-                    print ("Missing rules for this extensions: " + extension)
-            
+                if extension in filetypes: # check for existing rule in config
+                    os.rename(file, os.environ["HOME"] + config["ext"][extension] + filename)
+                    if quiet == False: # -q flag
+                        os.system("notify-send " + config["ext"][extension] + filename) 
+                else: 
+                    if debug == True:
+                        print ("Missing rules for this extensions: " + extension)
+        time.sleep(3)
 
 def run():
-    while True:
-        time.sleep(3)
+    with daemon.DaemonContext():
         move(sys.argv[1])
 
 if __name__ == "__main__":
